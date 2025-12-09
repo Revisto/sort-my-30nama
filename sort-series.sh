8fd7e88d1428fb38d5806a72110c25de44723d38
revisto <therevisto@gmail.com>
2025-12-09 12:20:00 +0330

    Fix regex capturing groups in sort-series.sh for improved season and quality extraction

--- sort-series.sh
+++ sort-series.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-pattern='(.+)_S([0-9]{2,})E([0-9]{2,}).*_([0-9]{3,})p_.*_30[Nn][Aa][Mm][Aa]\.(mkv|mp4)'
+pattern='(.+)_(S([0-9]{2,}))?E([0-9]{2,}).*_([0-9]{3,})p_.*_30[Nn][Aa][Mm][Aa]\.(mkv|mp4)'
 
 for file in *; do
     if [[ -f "$file" ]]; then
         if [[ "$file" =~ $pattern ]]; then
             series_name="${BASH_REMATCH[1]}"
-            season="${BASH_REMATCH[2]}"
-            quality="${BASH_REMATCH[4]}"
+            season="${BASH_REMATCH[3]}"
+            quality="${BASH_REMATCH[5]}"
+            
+            if [[ -z "$season" ]]; then
+                season="01"
+            fi
             
             series_path="${series_name}/S${season}/${quality}p"
             
